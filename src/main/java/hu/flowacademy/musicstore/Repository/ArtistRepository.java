934516629ab45a2ce7753b585387d87590adc9d4
Jozsef Urban <jozsef.urban.88@gmail.com>
2020-01-28 16:26:13 +0100

    repositorys2

--- src/main/java/hu/flowacademy/musicstore/Repository/ArtistRepository.java
+++ src/main/java/hu/flowacademy/musicstore/Repository/ArtistRepository.java
@@ -2,6 +2,8 @@ package hu.flowacademy.musicstore.Repository;
 
 import hu.flowacademy.musicstore.Model.Artist;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ArtistRepository extends JpaRepository<Artist, Long> {
 }
